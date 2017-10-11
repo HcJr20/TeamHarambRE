12707b9ab5ca0199116d015fb28e67355ea5949a
Ana Garcia <ajgarcia09@Anas-MacBook-Air.local>
2017-10-11 14:20:50 -0600

    changed the file name from passcode5 to Passphrase 5 for consistency

---
+++ Passphrase5.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Passphrase5 {
+
+	public void main() throws FileNotFoundException {
+		File f = new File("file.txt");
+		Scanner sc = new Scanner(f);
+		if (f.exists() && !f.isDirectory()) { // if file exist's
+
+			char var;
+			int i = 0;
+			boolean mistake = false;
+			while(sc.hasNext() && mistake == false){
+				var = sc.next().charAt(i);
+				if(hasMistake(var,i)){
+					mistake = true;
+					continue;
+				}
+				i++;
+			}
+			if(mistake)
+				System.out.println("Sorry the earth has been destroyed... Just kidding, dont give up!");
+			else{
+				System.out.println("Trigger 5 defused. Last one dont give up! What is it?");
+			}
+		}else{
+			System.out.println("Sorry the earth has been destroyed... Just kidding, dont give up!");
+		}
+		sc.close();
+	}
+
+	boolean hasMistake(char var, int pos){
+		boolean ret = true;
+
+		if(var == 'g' && pos == 0)
+			ret = false;
+		else if(var == 'o' && pos == 3 || pos == 8 || pos == 41)
+			ret = false;
+		else if(var == 'd' && pos == 17)
+			ret = false;
+		else if(var == 'j' && pos == 28)
+			ret = false;
+		else if(var == 'b' && pos == 56)
+			ret = false;
+		else if(var == ' ' && pos != 3 || pos != 8 || pos != 41 || pos != 17 || pos != 28 || pos != 56)
+			ret = false;
+
+		return ret;
+	}
+}
